60ef711625f7f05bc040dd3ac39dfaff0e4a2255
CaedenHarper <harpercaeden@gmail.com>
2024-02-08 00:43:25 -0800

    delete snake; fix pills bug; add pay timer

--- src/timer/index.js
+++ src/timer/index.js
@@ -1,7 +1,10 @@
 const countDownDate = new Date('Feb 8, 2024 19:10:00').getTime();
+const payCheckDate = new Date('Feb 14, 2024 00:00:00').getTime();
 // let countDownDate = new Date('Sep 12, 2023 9:09:00').getTime();
 const main_countdown = document.getElementById('time');
-const second_countdown = document.getElementById('hours');
+const main_secondary = document.getElementById('hours');
+const pay_countdown = document.getElementById('pay_time');
+// const pay_secondary = document.getElementById('pay_hours');
 const celebration_emoji = String.fromCodePoint(127881);
 const all_units = ['hours', 'minutes', 'seconds', 'fortnights', 'dog_hours',
 'usain_bolt_100_meters', 'pizza_hut', 'chopin', 'pills'];
@@ -13,6 +16,12 @@ let days = 0;
 let hours = 0;
 let minutes = 0;
 let seconds = 0;
+
+let pay_distance = 0;
+let pay_days = 0;
+let pay_hours = 0;
+let pay_minutes = 0;
+let pay_seconds = 0;
 
 // TODO: refactor; make dynamic/automatic
 
@@ -30,30 +39,45 @@ function display_countdown() {
     }
 }
 
+function display_pay() {
+    if (pay_distance < 0) {
+        // adc automatic system
+        pay_countdown.textContent = celebration_emoji;
+    } else if (pay_minutes <= 0 && pay_hours <= 0 && pay_days <= 0) {
+        pay_countdown.textContent = `${pay_seconds}s `;
+    } else if (pay_hours <= 0 && pay_days <= 0) {
+        pay_countdown.textContent = `${pay_minutes}m ${pay_seconds}s`;
+    } else if (pay_days <= 0) {
+        pay_countdown.textContent = `${pay_hours}h ${pay_minutes}m ${pay_seconds}s`;
+    } else {
+        pay_countdown.textContent = `${pay_days}d ${pay_hours}h ${pay_minutes}m ${pay_seconds}s`;
+    }
+}
+
 function display_hours() {
     const full_hours = days * 24 + hours;
-    if (distance < 0 || full_hours <= 0) {
-        second_countdown.textContent = '';
+    if (distance < 0) {
+        main_secondary.textContent = '';
     } else {
-        second_countdown.textContent = `(${full_hours} hours)`;
+        main_secondary.textContent = `(${full_hours} hours)`;
     }
 }
 
 function display_minutes() {
     const full_minutes = days * 24 * 60 + hours * 60 + minutes;
-    if (distance < 0 || full_minutes <= 0) {
-        second_countdown.textContent = '';
+    if (distance < 0) {
+        main_secondary.textContent = '';
     } else {
-        second_countdown.textContent = `(${full_minutes} minutes)`;
+        main_secondary.textContent = `(${full_minutes} minutes)`;
     }
 }
 
 function display_seconds() {
     const full_seconds = days * 24 * 60 * 60 + hours * 60 * 60 + minutes * 60 + seconds;
-    if (distance < 0 || full_seconds <= 0) {
-        second_countdown.textContent = '';
+    if (distance < 0) {
+        main_secondary.textContent = '0 seconds';
     } else {
-        second_countdown.textContent = `(${full_seconds} seconds)`;
+        main_secondary.textContent = `(${full_seconds} seconds)`;
     }
 }
 
@@ -62,10 +86,10 @@ function display_fortnights() {
     const full_seconds = days * 24 * 60 * 60 + hours * 60 * 60 + minutes * 60 + seconds;
     const fortnight_constant = 1210000;
     const num_fortnights = (full_seconds / (1.0 * fortnight_constant)).toFixed(4);
-    if (distance < 0 || num_fortnights <= 0) {
-        second_countdown.textContent = '';
+    if (distance < 0) {
+        main_secondary.textContent = '';
     } else {
-        second_countdown.textContent = `(${num_fortnights} fortnights)`;
+        main_secondary.textContent = `(${num_fortnights} fortnights)`;
     }
 }
 
@@ -73,49 +97,49 @@ function display_dog_hours() {
     const full_seconds = days * 24 * 60 * 60 + hours * 60 * 60 + minutes * 60 + seconds;
     const num_dog_seconds = (full_seconds * 7);
     const num_dog_hours = Math.round(num_dog_seconds / (60 * 60));
-    if (distance < 0 || num_dog_hours <= 0) {
-        second_countdown.textContent = '';
+    if (distance < 0) {
+        main_secondary.textContent = '';
     } else {
-        second_countdown.textContent = `(${num_dog_hours} dog hours)`;
+        main_secondary.textContent = `(${num_dog_hours} dog hours)`;
     }
 }
 
 function display_usain_bolt_100_meters() {
     const full_seconds = days * 24 * 60 * 60 + hours * 60 * 60 + minutes * 60 + seconds;
     const num_usain_bolt = Math.round(full_seconds / (9.58));
-    if (distance < 0 || num_usain_bolt <= 0) {
-        second_countdown.textContent = '';
+    if (distance < 0) {
+        main_secondary.textContent = '';
     } else {
-        second_countdown.textContent = `(${num_usain_bolt} Usain Bolt 100 meters)`;
+        main_secondary.textContent = `(${num_usain_bolt} Usain Bolt 100 meters)`;
     }
 }
 
 function display_pizza_hut() {
     const full_seconds = days * 24 * 60 * 60 + hours * 60 * 60 + minutes * 60 + seconds;
     const num_pizza_hut = Math.round(full_seconds / (15 * 60));
-    if (distance < 0 || num_pizza_hut <= 0) {
-        second_countdown.textContent = '';
+    if (distance < 0) {
+        main_secondary.textContent = '';
     } else {
-        second_countdown.textContent = `(${num_pizza_hut} Pizza Hut orders)`;
+        main_secondary.textContent = `(${num_pizza_hut} Pizza Hut orders)`;
     }
 }
 
 function display_chopin() {
     const full_seconds = days * 24 * 60 * 60 + hours * 60 * 60 + minutes * 60 + seconds;
     const num_chopin_hours = Math.round(2 * (full_seconds / (60 * 60)));
-    if (distance < 0 || num_chopin_hours <= 0) {
-        second_countdown.textContent = '';
+    if (distance < 0) {
+        main_secondary.textContent = '';
     } else {
-        second_countdown.textContent = `(${num_chopin_hours} Chopin hours)`;
+        main_secondary.textContent = `(${num_chopin_hours} Chopin hours)`;
     }
 }
 
 function display_pills() {
     const num_pills = 16 * days;
-    if (distance < 0 || num_pills <= 0) {
-        second_countdown.textContent = '';
+    if (distance < 0) {
+        main_secondary.textContent = '';
     } else {
-        second_countdown.textContent = `(${num_pills} pills)`;
+        main_secondary.textContent = `(${num_pills} pills)`;
     }
 }
 
@@ -185,12 +209,19 @@ function next_unit() {
 }
 
 // second_countdown.addEventListener('click', randomize_unit);
-second_countdown.addEventListener('click', next_unit);
+main_secondary.addEventListener('click', next_unit);
 
+// REFACTOR!!!!!
 function main() {
     // Get distance from now to date
     const now = new Date().getTime();
     distance = countDownDate - now;
+    pay_distance = payCheckDate - now;
+
+    // while pay_distance is in the past, add 14 days
+    while (pay_distance <= 0) {
+        pay_distance += 1209600000;
+    }
 
     // Time calculations for days, hours, minutes and seconds
     days = Math.floor(distance / (1000 * 60 * 60 * 24));
@@ -198,8 +229,14 @@ function main() {
     minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+    pay_days = Math.floor(pay_distance / (1000 * 60 * 60 * 24));
+    pay_hours = Math.floor((pay_distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    pay_minutes = Math.floor((pay_distance % (1000 * 60 * 60)) / (1000 * 60));
+    pay_seconds = Math.floor((pay_distance % (1000 * 60)) / 1000);
+
     display_countdown();
     current_unit_display();
+    display_pay();
 }
 
 setInterval(main, 1000);
